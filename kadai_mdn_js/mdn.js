cefaaec263fe43f15b8bab49b9afed5c8db8e9a0
araragi8608 <ran0872auga@gmail.com>
2025-08-06 22:15:26 +0900

    追加

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,6 @@
+const targetDate = new Date(2024, 9, 12);
+const year = targetDate.getFullYear();
+const month = targetDate.getMonth() + 1;
+const day = targetDate.getDate();
+
+console.log(`${year}年${month}月${day}日`);
